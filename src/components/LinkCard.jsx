2771e2d21b0fe390c34e7cb3d77c583403c8fd11
mlutfifahreza <mlutfifahreza@gmail.com>
2024-07-06 22:22:13 +0700

    feat: improvement product detail + navbar

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -4,7 +4,7 @@ const LinkCard = ({ link }) => {
   console.log("link.store_color", link.store_color);
   return (
     <a href={link.link} target="_blank" rel="noopener noreferrer">
-      <div className="mt-4 p-6 rounded-md shadow-md bg-sky-100 border-sky-500 border-2">
+      <div className="mt-4 p-6 rounded-md shadow-md  border-black border-2">
         <div className="flex items-left justify-left">
           <img src={link.store_icon} className="h-7 w-auto mr-2" />
           <div className="text-xl text-center">{link.store_name}</div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
       : "text-white hover:bg-sky-900 hover:text-white rounded-md px-3 py-2";
 
   return (
-    <nav className="bg-sky-700 border-b border-sky-500">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <nav className="bg-sky-700 border-b border-sky-500 mb-8">
+      <div className="mx-auto max-w-7xl">
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
