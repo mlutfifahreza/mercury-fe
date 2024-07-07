4940741eadbfa30a9821bdc1d884a1e0073afe88
mlutfifahreza <mlutfifahreza@gmail.com>
2024-07-07 15:54:02 +0700

    feat: product list page

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
+import ProductListPage from "./pages/ProductListPage";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/products" element={<ProductListPage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
