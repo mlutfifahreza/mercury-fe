acd826b8fa269f2f3e5ed122173c4e2c863b534a
mlutfifahreza <mlutfifahreza@gmail.com>
2024-07-07 15:53:45 +0700

    fix: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,17 +9,17 @@ const Navbar = () => {
 
   return (
     <nav className="bg-sky-700 border-b border-sky-500 mb-8">
-      <div className="mx-auto max-w-7xl">
+      <div className="mx-16 md:mx-auto md:max-w-7xl">
         <div className="flex h-20 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
+          <div className="flex flex-1 justify-start items-stretch">
+            <NavLink className="flex items-center mr-4" to="/">
               <img className="h-10 w-auto" src={logo} alt="React Jobs" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 Mercury List
               </span>
             </NavLink>
 
-            <div className="md:ml-auto">
+            <div className="ml-auto">
               <div className="flex space-x-2">
                 <NavLink to="/products" className={linkClass}>
                   Products
